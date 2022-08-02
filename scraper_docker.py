f84c9cdffebf10709ab184ef9f411485b5c27329
Raghul Sekar <104097352+RaghulSekar1997@users.noreply.github.com>
2022-08-02 03:05:24 +0100

    Update scraper_docker.py

--- scraper_docker.py
+++ scraper_docker.py
@@ -17,6 +17,8 @@ class Scraper:
 
         options = Options()
         options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
         s = ChromiumService(ChromeDriverManager().install())
 
 
