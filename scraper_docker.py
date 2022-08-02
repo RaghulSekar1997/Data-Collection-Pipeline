9e894fa63aa76ff750d8f7ebbe666d57e1e43b15
Raghul Sekar <104097352+RaghulSekar1997@users.noreply.github.com>
2022-08-02 04:00:48 +0100

    Update scraper_docker.py

--- scraper_docker.py
+++ scraper_docker.py
@@ -19,6 +19,11 @@ class Scraper:
         options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
+        
+        options.add_argument("--disable-gpu")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument("--remote-debugging-port=9222")
         s = ChromiumService(ChromeDriverManager().install())
 
 
